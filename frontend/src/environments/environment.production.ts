5b5cc41eb19486485c712ad669974f15fca392a4
RahulKumar1119 <suratirahulkumar29@gmail.com>
2026-07-19 10:56:22 +0530

    fix: include environment files in repo for Amplify build

---
+++ frontend/src/environments/environment.production.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.indifferent.fun',
+  googleClientId: '542018054302-hve0b3gf02gsvq9fojn0333o3007t33n.apps.googleusercontent.com',
+  googleRedirectUri: 'https://indifferent.fun/auth/callback',
+};
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000',
+  googleClientId: '542018054302-hve0b3gf02gsvq9fojn0333o3007t33n.apps.googleusercontent.com',
+  googleRedirectUri: 'http://localhost:4200/auth/callback',
+};
